533a3387a33105543e0c261339945e3bc5b773a6
Andrew Rodgers <acedrew@gmail.com>
2014-06-07 23:02:36 +0000

    added get_temperature method to mport class, created a small caching timeout, to reduce requests during testing

--- mfi.py
+++ mfi.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 
 class MfiDevice:
@@ -10,11 +11,14 @@ class MfiDevice:
         self.user = user
         self.passwd = passwd
         self.session = requests.Session()
+        self.data_retrieved = 0
+        #This get is necessary to set a cookie in the session prior to trying
+        #to login might be better to stick it in the login method itself
         self.session.get(url)
         self.login()
 
     def login(self):
-        post_data = {"uri": "mfi/sensors.cgi", "username": self.user,
+        post_data = {"uri": "/", "username": self.user,
                      "password": self.passwd}
         headers = {"Expect": ""}
         self.session.post((self.url + "/login.cgi"),
@@ -22,8 +26,10 @@ class MfiDevice:
                           allow_redirects=True)
 
     def get_data(self):
-        r = self.session.get((self.url + "/mfi/sensors.cgi"))
-        self.data = r.json()
+        if (time.time() - self.data_retrieved) > 2000:
+            r = self.session.get((self.url + "/mfi/sensors.cgi"))
+            self.data_retrieved = time.time()
+            self.data = r.json()
         return self.data
 
 
@@ -41,5 +47,17 @@ class MPower(MfiDevice):
 class MPort(MfiDevice):
     """Provides an API to a single mPort Device"""
 
-    def get_temperature(self, format='c'):
+    def get_temperature(self, port_no, temp_format='c'):
         self.get_data()
+        try:
+            sensor = self.data['sensors'][port_no - 1]
+            if "model" in sensor and sensor['model'] == 'Ubiquiti mFi-THS':
+                if temp_format == "c":
+                    return sensor['analog'] * 30 - 10
+                elif temp_format == "f":
+                    return (sensor['analog'] * 30 - 10) * 1.8 + 32
+            else:
+                raise IndexError
+        except(IndexError):
+            print("Sorry port #" + str(port_no) +
+                    " either does not exist or is not a Temperature Sensor")
--- test.py
+++ test.py
@@ -3,9 +3,12 @@ import os
 
 test_mpower = mfi.MPower(os.environ['TESTMPOWER'], os.environ['TESTUSER'],
                          os.environ['TESTPASS'])
-test_mport = mfi.MPower(os.environ['TESTMPORT'], os.environ['TESTUSER'],
+test_mport = mfi.MPort(os.environ['TESTMPORT'], os.environ['TESTUSER'],
                         os.environ['TESTPASS'])
 print(test_mpower.get_data())
 print(test_mpower.get_power(1))
 print(test_mpower.get_power(2))
-print(test_mport.get_power(1))
+print(test_mport.get_data())
+print(test_mport.get_temperature(1))
+print(test_mport.get_temperature(1, 'f'))
+print(test_mport.get_temperature(2))
